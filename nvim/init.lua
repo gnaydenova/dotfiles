a76839a53f4d9aa53c33a4bbf3e9aaa45258de25
Gergana Naydenova <gtnaydenova@gmail.com>
2022-12-02 18:48:36 +0200

    add gitsigns, signature-help, catppuccin theme, autopairs, overall update of conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,26 +2,75 @@ require('core.opts')
 
 require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
+	use { "catppuccin/nvim", as = "catppuccin", config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato"
+		})
+		vim.cmd.colorscheme "catppuccin"
+	end
+	}
 	use {
 		'ful1e5/onedark.nvim',
+		disable = true,
 		config = function()
 			require('onedark').setup {
-				overrides = function(colors)
+				overrides = function()
 					return {
 						Visual = { style = "reverse" }
 					}
 				end
 			}
-			vim.cmd[[colo onedark]]
+			vim.cmd [[colo onedark]]
 		end
 	}
 	use {
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			local gitsigns = require "gitsigns"
+
+			gitsigns.setup {
+				signs = {
+					add = { text = "│" },
+					change = { text = "│" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+				keymaps = {
+					noremap = true,
+					buffer = true,
+					["n ]c"] = {
+						expr = true,
+						"&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
+					},
+					["n [c"] = {
+						expr = true,
+						"&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
+					},
+				},
+				sign_priority = 5,
+				current_line_blame = true,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 200,
+					ignore_whitespace = true,
+				},
+				current_line_blame_formatter_opts = {
+					relative_time = true,
+				},
+			}
+		end,
+		requires = { "nvim-lua/plenary.nvim" },
+	}
+
+	use {
 		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		requires = { 'kyazdani42/nvim-web-devicons' },
 		config = function()
 			require('lualine').setup {
 				options = {
-					theme = 'onedark-nvim',
+					theme = 'catppuccin',
 				}
 			}
 		end
@@ -37,8 +86,7 @@ require('packer').startup(function(use)
 			vim.cmd [[set foldmethod=expr]]
 			vim.cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
 			vim.cmd [[set foldlevel=10]]
-			require'nvim-treesitter.configs'.setup {
-				ensure_installed = "maintained",
+			require 'nvim-treesitter.configs'.setup {
 				highlight = {
 					enable = true
 				},
@@ -53,14 +101,61 @@ require('packer').startup(function(use)
 			}
 		end
 	}
+	use {
+		"windwp/nvim-autopairs",
+		disable = false,
+		config = function()
+			---@diagnostic disable: missing-parameter
+			local npairs = require "nvim-autopairs"
+			local Rule = require "nvim-autopairs.rule"
+
+			npairs.setup {
+				map_c_w = true,
+				map_c_h = true,
+				enable_check_bracket_line = false,
+			}
+
+			npairs.add_rules {
+				Rule(" ", " "):with_pair(function(opts)
+					local pair = opts.line:sub(opts.col - 1, opts.col)
+					return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+				end),
+				Rule("( ", " )")
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.prev_char:match ".%)" ~= nil
+					end)
+					:use_key ")",
+				Rule("{ ", " }")
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.prev_char:match ".%}" ~= nil
+					end)
+					:use_key "}",
+				Rule("[ ", " ]")
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.prev_char:match ".%]" ~= nil
+					end)
+					:use_key "]",
+			}
+		end,
+	}
+
 	use 'tpope/vim-surround'
 	use {
 		'nvim-telescope/telescope.nvim',
-		requires = {'nvim-lua/plenary.nvim'}
+		requires = { 'nvim-lua/plenary.nvim' }
 	}
 	use {
 		'neovim/nvim-lspconfig',
-		requires = {'williamboman/nvim-lsp-installer'},
+		requires = { 'williamboman/nvim-lsp-installer' },
 		config = function()
 			require('plugins.lsp')
 		end
@@ -77,30 +172,55 @@ require('packer').startup(function(use)
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
 		},
 		config = function()
-			local cmp = require'cmp'
+			local cmp = require 'cmp'
 			local luasnip = require("luasnip")
 
 			luasnip.config.set_config({ history = true })
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
+			if not pcall(require, "nvim-autopairs.completion.cmp") then
+				cmp.event:on(
+					"confirm_done",
+					require("nvim-autopairs.completion.cmp").on_confirm_done { map_char = { text = "" } }
+				)
+			end
+
 			local has_words_before = function()
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
-			cmp.setup{
+			cmp.setup {
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body) -- For `luasnip` users.
 					end
 				},
+				view = {
+					entries = "custom",
+					selection_order = "near_cursor",
+				},
+				experimental = { native_menu = false, ghost_text = true },
+				sortings = {
+					comparators = {
+						cmp.config.compare.offset,
+						cmp.config.compare.exact,
+						cmp.config.compare.score,
+						cmp.config.compare.kind,
+						cmp.config.compare.sort_text,
+						cmp.config.compare.length,
+						cmp.config.compare.order,
+					},
+				},
 				sources = cmp.config.sources({
-					{name = 'nvim_lsp'},
-					{name = "luasnip"},
-					{name = 'path'},
-					{name = 'buffer'}
+					{ name = 'nvim_lsp' },
+					{ name = "luasnip" },
+					{ name = 'nvim_lsp_signature_help' },
+					{ name = 'path' },
+					{ name = 'buffer' }
 				}),
 				mapping = {
 					["<Tab>"] = cmp.mapping(function(fallback)
@@ -127,6 +247,58 @@ require('packer').startup(function(use)
 					['<CR>'] = cmp.mapping.confirm({ select = true }),
 				}
 			}
+		end
+	}
+	use {
+		"tamago324/lir.nvim",
+		disable = false,
+		requires = {
+			"nvim-lua/plenary.nvim",
+			-- "tamago324/lir-git-status.nvim",
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			local actions = require "lir.actions"
+			local mark_actions = require "lir.mark.actions"
+			local clipboard_actions = require "lir.clipboard.actions"
+
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+
+			require("lir").setup {
+				show_hidden_files = true,
+				devicons_enable = true,
+				mappings = {
+					["<CR>"] = actions.edit,
+					["l"] = actions.edit,
+					["<C-s>"] = actions.split,
+					["<C-v>"] = actions.vsplit,
+					["<C-t>"] = actions.tabedit,
+
+					["h"] = actions.up,
+					["-"] = actions.up,
+					["q"] = actions.quit,
+
+					["K"] = actions.mkdir,
+					["N"] = actions.newfile,
+					["R"] = actions.rename,
+					["@"] = actions.cd,
+					["Y"] = actions.yank_path,
+					["."] = actions.toggle_show_hidden,
+					["D"] = actions.delete,
+
+					["J"] = function()
+						mark_actions.toggle_mark("n")
+					end,
+					["C"] = clipboard_actions.copy,
+					["X"] = clipboard_actions.cut,
+					["P"] = clipboard_actions.paste,
+				},
+				hide_cursor = false,
+			}
+
+			vim.keymap.set("n", "§", "<CMD>e .<CR>", { desc = "Open current working directory" })
+			vim.keymap.set("n", "-", "<CMD>e %:p:h<CR>", { desc = "Open parent directory" })
 		end
 	}
 end)
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,35 +1,72 @@
-local function on_attach(client, bufnr)
-	vim.opt_local.tagfunc = "v:lua.vim.lsp.tagfunc"
-	vim.opt_local.formatexpr = "v:lua.vim.lsp.formatexpr()"
+local lspconfig = require "lspconfig"
 
-	local opts = { noremap=true, silent=true }
+local function on_attach(_, bufnr)
+	vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+    vim.opt_local.tagfunc = "v:lua.vim.lsp.tagfunc"
+    vim.opt_local.formatexpr = "v:lua.vim.lsp.formatexpr()"
+
+	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "single",
+        max_width = 80,
+    })
+
+
+	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
+		'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
 end
 
 local config = {
-	sumneko_lua = require("lua-dev").setup {}
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				completion = {
+					callSnippet = "Replace"
+				}
+			}
+		}
+	}
 }
 
-require('nvim-lsp-installer').on_server_ready(function(server)
+require("nvim-lsp-installer").setup {
+    automatic_installation = true,
+    ensure_installed = { "sumneko_lua" },
+}
+
+local function setup_server(server)
 	local opts = {
 		on_attach = on_attach,
-		capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
 	}
-	if config[server.name] then
-		opts = vim.tbl_extend('force', opts, config[server.name])
+
+	if server.name == "jsonls" then
+		opts.capabilities.textDocument.completion.completionItem.snippetSupport = true
 	end
 
-	server:setup(opts)
-end)
+	if server.name == "sumneko_lua" then
+		require("neodev").setup({})
+	end
+
+	if config[server.name] then
+		---@diagnostic disable-next-line: cast-local-type
+		opts = vim.tbl_extend("force", opts, config[server.name])
+	end
+
+	lspconfig[server.name].setup(opts)
+end
+
+for _, server in ipairs(require("nvim-lsp-installer.servers").get_installed_servers()) do
+	setup_server(server)
+end
