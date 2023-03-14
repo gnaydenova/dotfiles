a33a8c51bd47dbc85cb98a619c14cbfeaa380764
Gergana Naydenova <gtnaydenova@gmail.com>
2023-03-14 12:32:57 +0200

    add neotest

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ return {
 	config = function()
 		local lsp = require('lsp-zero').preset({
 			name = 'recommended',
-			set_lsp_keymaps = { omit = { '<F2>', '<F4>' } },
+			set_lsp_keymaps = false,
 			manage_nvim_cmp = true,
 			suggest_lsp_servers = true,
 		})
@@ -29,6 +29,13 @@ return {
 			local opts = { buffer = bufnr }
 			local bind = vim.keymap.set
 
+			bind('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+			bind('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+			bind('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+			bind('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+			bind('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+			bind('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+			bind('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
 			bind('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
 			bind('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 			bind('n', '<leader>f', '<cmd>LspZeroFormat<cr>', opts)
